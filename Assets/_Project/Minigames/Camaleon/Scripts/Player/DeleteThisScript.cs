8766df824b4aef0edb59d23c63f27ff7c7756a2a
OscarSanschez <a01178771@tec.mx>
2026-03-10 12:59:12 -0600

    ttt

---
+++ Assets/_Project/Minigames/Camaleon/Scripts/Player/DeleteThisScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DeleteThisScript : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Debug.Log("This script is not used anymore, please delete it.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
