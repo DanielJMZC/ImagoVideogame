c8f0e7805bb7e1e7dbc467270cad729b3fe91d97
ItsKylos <A01723532@tec.mx>
2026-04-28 21:59:38 -0600

    URL URL

--- Assets/_Project/Minigames/Dudas/Scripts/DialogService.cs
+++ Assets/_Project/Minigames/Dudas/Scripts/DialogService.cs
@@ -14,8 +14,8 @@ public class DialogService : MonoBehaviour
 
     public IEnumerator GetDialogos(int npcId, System.Action<List<Dialogo>> callback)
     {
-        string url = $"http://127.0.0.1:5530/npc/{npcId}/dialogos";
-        //string url = $"http://10.14.255.43:5530/npc/{npcId}/dialogos";
+        //string url = $"http://127.0.0.1:5530/npc/{npcId}/dialogos";
+        string url = $"http://10.14.255.43:5530/npc/{npcId}/dialogos";
 
         UnityWebRequest request = UnityWebRequest.Get(url);
         yield return request.SendWebRequest();
@@ -39,8 +39,8 @@ public class DialogService : MonoBehaviour
 
     public IEnumerator GetPreguntas(int npcId, System.Action<List<Pregunta>> callback)
     {
-        string url = $"http://127.0.0.1:5530/npc/{npcId}/preguntas";
-        //string url = $"http://10.14.255.43:5530/npc/{npcId}/preguntas";
+        //string url = $"http://127.0.0.1:5530/npc/{npcId}/preguntas";
+        string url = $"http://10.14.255.43:5530/npc/{npcId}/preguntas";
 
         UnityWebRequest request = UnityWebRequest.Get(url);
         yield return request.SendWebRequest();
